e888fbd86d94dfea45177a9a6deb31f6b860541d
Katie Zhou <katiezhou@Katies-MacBook-Pro.local>
2015-11-05 22:10:43 -0500

    adding comment functionality, showing likes and comments on songpost show page

--- project2_app/app/controllers/comments_controller.rb
+++ project2_app/app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+	skip_before_action :verify_authenticity_token, only: [:create, :new]
+	def new
+		@comment = Comment.new
+	end
+
+	def create
+		@comment = Comment.create(comment_params)
+
+		redirect_to(:back)
+	end
+
+	private
+
+	def comment_params
+		params.require(:comment).permit(:songpost_id, :user_id, :content)
+	end
+
 end
